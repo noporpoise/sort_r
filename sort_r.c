4bf78a3f0bf832d53b6ba03845b59e84d87c4f78
Isaac Turner <turner.isaac@gmail.com>
2013-02-10 18:24:23 +0000

    Compiles on mac with -pedantic

--- sort_r.c
+++ sort_r.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <search.h>
 
 struct sort_r_data
 {
@@ -24,7 +25,9 @@ void sort_r(void *base, size_t nel, size_t width,
          defined __FREEBSD__ || defined __BSD__ || \
          defined OpenBSD3_1 || defined OpenBSD3_9)
 
-    struct sort_r_data tmp = {arg, compar};
+    struct sort_r_data tmp;
+    tmp.arg = arg;
+    tmp.compar = compar;
     qsort_r(base, nel, width, &tmp, &sort_r_arg_swap);
 
   #elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__)
@@ -38,11 +41,11 @@ void sort_r(void *base, size_t nel, size_t width,
 }
 
 
-// Example
+/* Example */
 
-// comparison function to sort an array of int, inverting a given region
-// `arg` should be of type int[2], with the elements
-// representing the start and end of the region to invert (inclusive)
+/* comparison function to sort an array of int, inverting a given region
+   `arg` should be of type int[2], with the elements
+   representing the start and end of the region to invert (inclusive) */
 int sort_r_cmp(const void *aa, const void *bb, void *arg)
 {
   const int *a = aa, *b = bb, *p = arg;
@@ -54,13 +57,13 @@ int sort_r_cmp(const void *aa, const void *bb, void *arg)
 
 int main()
 {
-  // sort 1..19, 30..20, 30..100
+  int i;
+  /* sort 1..19, 30..20, 30..100 */
   int arr[18] = {1, 5, 28, 4, 3, 2, 10, 20, 18, 25, 21, 29, 34, 35, 14, 100, 27, 19};
-  // Region to invert: 20-30 (inclusive)
+  /* Region to invert: 20-30 (inclusive) */
   int p[] = {20, 30};
   sort_r(arr, 18, sizeof(int), sort_r_cmp, p);
-  
-  int i;
+
   for(i = 0; i < 18; i++) printf(" %i", arr[i]);
   printf("\n");
   return 0;
