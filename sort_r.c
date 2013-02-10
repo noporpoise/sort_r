856ccb40e4a0ccd7630788b3324b0d60bd9cf73f
Isaac Turner <turner.isaac@gmail.com>
2013-02-10 18:53:13 +0000

    Moved sort_r function def to top of file

--- sort_r.c
+++ sort_r.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <search.h>
 
+/* sort_r function to be exported */
+void sort_r(void *base, size_t nel, size_t width,
+            int (*compar)(const void *a1, const void *a2, void *aarg), void *arg);
+
 #ifdef QSORT_WITH_NESTED_FUNCTIONS
 
 void sort_r(void *base, size_t nel, size_t width,
