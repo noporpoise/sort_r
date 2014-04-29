7c60ef94753624291055af0b1eec291f8c4bd5a7
Isaac Turner <turner.isaac@gmail.com>
2014-04-29 19:12:47 +0100

    BUG fix + re-write, no change to API; Better tests for OS

--- sort_r.h
+++ sort_r.h
@@ -1,4 +1,7 @@
-/* Isaac Turner 23 Jan 2014 Public Domain */
+/* Isaac Turner 29 April 2014 Public Domain */
+#ifndef SORT_R_H_
+#define SORT_R_H_
+
 #include <stdlib.h>
 
 /*
@@ -12,90 +15,108 @@ Parameters:
   arg is a pointer to be passed to the comparison function
 
 void sort_r(void *base, size_t nel, size_t width,
-            int (*compar)(const void *a1, const void *a2, void *aarg),
+            int (*compar)(const void *_a, const void *_b, void *_arg),
             void *arg);
 */
 
-#if (defined NESTED_QSORT && NESTED_QSORT == 0)
-  #undef NESTED_QSORT
-#elif (!defined(NESTED_QSORT) && \
-       (defined(__MINGW32__) || defined(__OpenBSD__) || defined(AMIGA) || \
-        defined(__gnu_hurd__) || \
-        (defined(__GLIBC__) && __GLIBC__ == 2 && __GLIBC_MINOR__ < 8)))
-  /* operating systems without qsort_r/qsort_s perhaps? */
-  #define NESTED_QSORT 1
+#if (defined __APPLE__ || defined __MACH__ || defined __DARWIN__ || \
+     defined __FREEBSD__ || defined __BSD__ || \
+     defined OpenBSD3_1 || defined OpenBSD3_9 || defined __OpenBSD__ || \
+     defined AMIGA)
+#  define _SORT_R_BSD
+#elif (defined _GNU_SOURCE || defined __gnu_hurd__ || defined __GNU__ || \
+       defined __linux__ || defined __MINGW32__ || defined __GLIBC__)
+#  define _SORT_R_LINUX
+#elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__)
+#  define _SORT_R_WINDOWS
+#else
+#  error Cannot detect operating system
 #endif
+
+#if (defined NESTED_QSORT && NESTED_QSORT == 0)
+#  undef NESTED_QSORT
+#elif (!defined NESTED_QSORT && \
+       defined __GLIBC__ && __GLIBC__ == 2 && __GLIBC_MINOR__ < 8)
+/* no qsort_r in glibc before 2.8 */
+#  define NESTED_QSORT
+#endif
+
 
 #if defined NESTED_QSORT
 
-static inline void sort_r(void *base, size_t nel, size_t width,
-                          int (*compar)(const void *a1, const void *a2, void *aarg),
-                          void *arg)
-{
-  int nested_cmp(const void *a, const void *b)
+  static inline void sort_r(void *base, size_t nel, size_t width,
+                            int (*compar)(const void *_a, const void *_b, void *aarg),
+                            void *arg)
   {
-    return compar(a, b, arg);
+    int nested_cmp(const void *a, const void *b)
+    {
+      return compar(a, b, arg);
+    }
+
+    qsort(base, nel, width, nested_cmp);
   }
 
-  qsort(base, nel, width, nested_cmp);
-}
+#else /* !NESTED_QSORT */
 
-#else
+  /* Declare structs and functions */
+  #if defined _SORT_R_BSD
 
-/* Declare structs and functions */
+    /* BSD requires argument swap */
+    extern void qsort_r(void *base, size_t nel, size_t width, void *thunk,
+                        int (*compar)(void *_thunk, const void *_a, const void *_b));
 
-#if (defined _GNU_SOURCE || defined __GNU__ || defined __linux__)
+    struct sort_r_data
+    {
+      void *arg;
+      int (*compar)(const void *_a, const void *_b, void *_arg);
+    };
 
-typedef int(* __compar_d_fn_t)(const void *, const void *, void *);
-extern void qsort_r (void *__base, size_t __nmemb, size_t __size,
-                     __compar_d_fn_t __compar, void *__arg)
-  __attribute__((nonnull (1, 4)));
+    static inline int sort_r_arg_swap(void *s, const void *a, const void *b)
+    {
+      struct sort_r_data *ss = (struct sort_r_data*)s;
+      return (ss->compar)(a, b, ss->arg);
+    }
 
-#else /* Not GNU linux */
+  #elif defined _SORT_R_LINUX
 
-struct sort_r_data
-{
-  void *arg;
-  int (*compar)(const void *a1, const void *a2, void *aarg);
-};
+    typedef int(* __compar_d_fn_t)(const void *, const void *, void *);
+    extern void qsort_r(void *base, size_t nel, size_t width,
+                        __compar_d_fn_t __compar, void *arg)
+      __attribute__((nonnull (1, 4)));
 
-static inline int sort_r_arg_swap(void *s, const void *aa, const void *bb)
-{
-  struct sort_r_data *ss = (struct sort_r_data*)s;
-  return (ss->compar)(aa, bb, ss->arg);
-}
-
-#endif
-
-/* non-nested implementation */
-
-static inline void sort_r(void *base, size_t nel, size_t width,
-                          int (*compar)(const void *a1, const void *a2, void *aarg),
-                          void *arg)
-{
-  #if (defined _GNU_SOURCE || defined __GNU__ || defined __linux__)
-
-    qsort_r(base, nel, width, compar, arg);
-
-  #elif (defined __APPLE__ || defined __MACH__ || defined __DARWIN__ || \
-         defined __FREEBSD__ || defined __BSD__ || \
-         defined OpenBSD3_1 || defined OpenBSD3_9)
-
-    struct sort_r_data tmp;
-    tmp.arg = arg;
-    tmp.compar = compar;
-    qsort_r(base, nel, width, &tmp, &sort_r_arg_swap);
-
-  #elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__)
-
-    struct sort_r_data tmp;
-    tmp.arg = arg;
-    tmp.compar = compar;
-    qsort_s(base, nel, width, &sort_r_arg_swap, &tmp);
-
-  #else
-    #error Cannot detect operating system
   #endif
-}
+
+  /* implementation */
+
+  static inline void sort_r(void *base, size_t nel, size_t width,
+                            int (*compar)(const void *_a, const void *_b, void *_arg),
+                            void *arg)
+  {
+    #if defined _SORT_R_LINUX
+
+      qsort_r(base, nel, width, compar, arg);
+
+    #elif defined _SORT_R_BSD
+
+      struct sort_r_data tmp;
+      tmp.arg = arg;
+      tmp.compar = compar;
+      qsort_r(base, nel, width, &tmp, sort_r_arg_swap);
+
+    #else /* defined _SORT_R_WINDOWS */
+
+      struct sort_r_data tmp;
+      tmp.arg = arg;
+      tmp.compar = compar;
+      qsort_s(base, nel, width, sort_r_arg_swap, &tmp);
+
+    #endif
+  }
 
 #endif /* !NESTED_QSORT */
+
+#undef _SORT_R_WINDOWS
+#undef _SORT_R_LINUX
+#undef _SORT_R_BSD
+
+#endif /* SORT_R_H_ */
