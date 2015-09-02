50e6e86b035fcaf1610de0451a2b590ef5e39c64
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 17:03:56 +0100

    Travis CI: fix typo

--- travis.sh
+++ travis.sh
@@ -10,7 +10,7 @@ make && ./example_sort && make clean
 make NESTED_QSORT=0 && ./example_sort && make clean
 
 # linux+gcc is the only set up that supports nested functions
-if [ "$TRAVIS_OS_NAME" == "linux" && "$CC" == "gcc" ]
+if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$CC" == "gcc" ]
 then
   make NESTED_QSORT=1 && ./example_sort && make clean
 fi
