668e3ac0f3e1b70ce6b89d9b20a7ed2bae50fa7c
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 17:03:29 +0100

    Travis CI: ONLY run nested func tests on linux+gcc

--- travis.sh
+++ travis.sh
@@ -9,8 +9,8 @@ set -e
 make && ./example_sort && make clean
 make NESTED_QSORT=0 && ./example_sort && make clean
 
-# Clang doesn't support nested functions
-if [ "$CC" != "clang" ]
+# linux+gcc is the only set up that supports nested functions
+if [ "$TRAVIS_OS_NAME" == "linux" && "$CC" == "gcc" ]
 then
   make NESTED_QSORT=1 && ./example_sort && make clean
 fi
