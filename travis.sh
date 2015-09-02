75cf8c8ed4af6480f252bae688f0dcbf040660e3
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 16:59:12 +0100

    Travis CI: skip nested functions example on clang

--- travis.sh
+++ travis.sh
@@ -9,7 +9,8 @@ set -e
 make && ./example_sort && make clean
 make NESTED_QSORT=0 && ./example_sort && make clean
 
-if[ "$TRAVIS_OS_NAME" != "osx" || "$CC" != "clang" ]
+# Clang doesn't support nested functions
+if [ "$CC" != "clang" ]
 then
   make NESTED_QSORT=1 && ./example_sort && make clean
 fi
