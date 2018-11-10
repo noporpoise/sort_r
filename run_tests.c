1600aba542920feaba9dcdfe8f1a69772221631b
Isaac Turner <isaac.turner@shielddx.com>
2018-11-10 14:37:38 -0800

    Add run_tests.c file

---
+++ run_tests.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "sort_r.h"
+
+
+static int cmp_ints(const void *aa, const void *bb, void *arg)
+{
+  (void)arg;
+  const int a = *(const int*)aa, b = *(const int*)bb;
+  return (a > b ? 1 : -(a<b));
+}
+
+void check_sorted(const void *arr, size_t n, size_t w,
+                  int (*compar)(const void *_a, const void *_b, void *_arg),
+                  void *arg)
+{
+  const char *b = (const char*)arr, *end = b + n*w;
+  for(; b+w < end; b += w) {
+    if(compar(b, b+w, arg) > 0) {
+      /* error */
+      fprintf(stderr, "Error: list not sorted\n");
+      exit(-1);
+    }
+  }
+}
+
+void run_tests(int *arr, int n) {
+  /* All identical values */
+  for(int i = 0; i < n; i++) { arr[i] = 10; }
+  sort_r(arr, n, sizeof(int), cmp_ints, NULL);
+  check_sorted(arr, n, sizeof(int), cmp_ints, NULL);
+
+  /* Ascending values */
+  for(int i = 0; i < n; i++) { arr[i] = i; }
+  sort_r(arr, n, sizeof(int), cmp_ints, NULL);
+  check_sorted(arr, n, sizeof(int), cmp_ints, NULL);
+
+  /* Descending values */
+  for(int i = 0; i < n; i++) { arr[i] = n-i; }
+  sort_r(arr, n, sizeof(int), cmp_ints, NULL);
+  check_sorted(arr, n, sizeof(int), cmp_ints, NULL);
+
+  /* Random values */
+  for(int i = 0; i < n; i++) { arr[i] = rand(); }
+  sort_r(arr, n, sizeof(int), cmp_ints, NULL);
+  check_sorted(arr, n, sizeof(int), cmp_ints, NULL);
+}
+
+
+int main()
+{
+  srand(time(NULL));
+
+  int n = 10000000;
+  int *arr = malloc(n*sizeof(n));
+
+  for(int i = 0; i < 20; i++) {
+    run_tests(arr, i);
+  }
+
+  /* Run tests using the entire array */
+  run_tests(arr, n);
+
+  return 0;
+}
