0851d936dd57a676ec67578930bd40c2a22846ce
Isaac Turner <isaac.turner@shielddx.com>
2018-12-07 04:04:35 +0000

    Update TravisCI script

--- travis.sh
+++ travis.sh
@@ -4,13 +4,13 @@
 # Tests to run on Travis CI
 #
 
-set -e
+set -eou pipefail
 
-make && ./example_sort && make clean
-make NESTED_QSORT=0 && ./example_sort && make clean
+make && ./example && ./run_tests && make clean
+make NESTED_QSORT=0 && ./example && ./run_tests && make clean
 
 # linux+gcc is the only set up that supports nested functions
 if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$CC" == "gcc" ]
 then
-  make NESTED_QSORT=1 && ./example_sort && make clean
+  make NESTED_QSORT=1 && ./example && ./run_tests && make clean
 fi
