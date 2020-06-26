36f588038939dae216438718d9851f095548a14c
LudwigFriedmann <53524165+LudwigFriedmann@users.noreply.github.com>
2020-06-26 11:40:29 +0200

    Prevent MinGW from searching for glibc on Windows

--- sort_r.h
+++ sort_r.h
@@ -28,7 +28,7 @@ void sort_r(void *base, size_t nel, size_t width,
      defined __FreeBSD__ || defined __DragonFly__)
 #  define _SORT_R_BSD
 #elif (defined _GNU_SOURCE || defined __gnu_hurd__ || defined __GNU__ || \
-       defined __linux__ || defined __MINGW32__ || defined __GLIBC__)
+       defined __linux__ || defined __GLIBC__)
 #  define _SORT_R_LINUX
 #elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__)
 #  define _SORT_R_WINDOWS
