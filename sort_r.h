1275373d990162f7d142de2fcaf43b535061466c
Isaac Turner <turner.isaac@gmail.com>
2022-10-09 23:40:47 -0500

    Only rely on __GLIBC__ macro for qsort_r() (#18)
    * Add comment on import for memcpy()
    
    * Fix indentation
    
    * Only use glibc qsort_r() if __GLIB__ defined

--- sort_r.h
+++ sort_r.h
@@ -2,8 +2,8 @@
 #ifndef SORT_R_H_
 #define SORT_R_H_
 
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> /* qsort_r(), qsort_s() */
+#include <string.h> /* needed for memcpy() */
 
 /*
 
@@ -27,9 +27,7 @@ void sort_r(void *base, size_t nel, size_t width,
 #if (defined __APPLE__ || defined __MACH__ || defined __DARWIN__ || \
      (defined __FreeBSD__ && !defined(qsort_r)) || defined __DragonFly__)
 #  define _SORT_R_BSD
-#elif (defined _GNU_SOURCE || defined __gnu_hurd__ || defined __GNU__ || \
-       defined __linux__ || defined __GLIBC__ || \
-       (defined (__FreeBSD__) && defined(qsort_r)))
+#elif (defined __GLIBC__ || (defined (__FreeBSD__) && defined(qsort_r)))
 #  define _SORT_R_LINUX
 #elif (defined _WIN32 || defined _WIN64 || defined __WINDOWS__ || \
        defined __MINGW32__ || defined __MINGW64__)
@@ -266,7 +264,7 @@ static _SORT_R_INLINE void sort_r_simple(void *base, size_t nel, size_t w,
 
     typedef int(* __compar_d_fn_t)(const void *, const void *, void *);
     extern void (qsort_r)(void *base, size_t nel, size_t width,
-                        __compar_d_fn_t __compar, void *arg)
+                          __compar_d_fn_t __compar, void *arg)
       __attribute__((nonnull (1, 4)));
 
   #endif
